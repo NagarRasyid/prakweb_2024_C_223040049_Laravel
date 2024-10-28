0dce2214f827af3098b75e164f42a390ecab42f3
NagarRasyid <122325152+NagarRasyid@users.noreply.github.com>
2024-10-28 23:38:14 +0700

    Add files via upload

---
+++ storage/framework/views/71afbd0b17dbdfc24695c604a56f51be.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Blog</title>
+</head>
+<body>
+    <a href="/">Home</a>
+    <a href="/blog">Blog</a>
+    <a href="/about">About</a>
+    <a href="/contact">Contact</a>
+    <h1>Halaman Blog</h1>
+    <article>
+        <h3>Judul 1</h3>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatibus tenetur alias nostrum architecto, optio saepe magnam doloribus ipsam. Earum reprehenderit iure dolorum asperiores accusantium nam debitis enim similique quis iste.</p>
+    </article>
+
+    <article>
+        <h3>Judul 2</h3>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic sunt doloribus, corrupti eum quibusdam officia nobis adipisci dolor, voluptas vel nulla autem animi enim. Maxime totam odit excepturi repudiandae porro.</p>
+    </article>
+</body>
+</html><?php /**PATH C:\Users\anjay\Herd\laravel11\resources\views/blog.blade.php ENDPATH**/ ?>
